93236d180b4cbf0048ae29a3d4fbc9c46cd038a6
Mike Danaher <danaher.mike@gmail.com>
2014-05-08 16:04:55 -0500

    Refactored populator class

--- .tmp/scripts/populator.js
+++ .tmp/scripts/populator.js
@@ -2,19 +2,28 @@
   namespace('FormsJs');
 
   FormsJs.Populator = (function() {
-    var setAllChecked, setChecked, setValue;
-
     function Populator() {}
 
-    setValue = function(data) {
+    Populator.populate = function(data) {
+      switch (data.type) {
+        case FormsJs.InputTypes.RADIO:
+          return this.setChecked(data);
+        case FormsJs.InputTypes.CHECKBOX:
+          return this.setAllChecked(data);
+        default:
+          return this.setValue(data);
+      }
+    };
+
+    Populator.setValue = function(data) {
       return $("[name='" + data.name + "']").val(data.value);
     };
 
-    setChecked = function(data) {
+    Populator.setChecked = function(data) {
       return $("[name='" + data.name + "'][value='" + data.value + "']").prop('checked', true);
     };
 
-    setAllChecked = function(data) {
+    Populator.setAllChecked = function(data) {
       var value;
       if (_.isArray(data.value)) {
         value = data.value;
@@ -22,23 +31,6 @@
         value = [data.value];
       }
       return $("[name='" + data.name + "']").val(value);
-    };
-
-    Populator.populate = function(data) {
-      switch (data.type) {
-        case FormsJs.InputTypes.TEXT:
-          return setValue(data);
-        case FormsJs.InputTypes.RADIO:
-          return setChecked(data);
-        case FormsJs.InputTypes.CHECKBOX:
-          return setAllChecked(data);
-        case FormsJs.InputTypes.SELECT:
-          return setValue(data);
-        case FormsJs.InputTypes.PASSWORD:
-          return setValue(data);
-        default:
-          return setValue(data);
-      }
     };
 
     return Populator;
--- scripts/populator.coffee
+++ scripts/populator.coffee
@@ -2,24 +2,23 @@ namespace('FormsJs')
 
 class FormsJs.Populator
 
-  setValue = (data) ->
+  @populate: (data) ->
+    switch data.type
+      when FormsJs.InputTypes.RADIO then @setChecked(data)
+      when FormsJs.InputTypes.CHECKBOX then @setAllChecked(data)
+      else @setValue(data)
+
+  @setValue = (data) ->
     $("[name='#{data.name}']").val(data.value)
 
-  setChecked = (data) ->
+  @setChecked = (data) ->
     $("[name='#{data.name}'][value='#{data.value}']").prop('checked', true)
 
-  setAllChecked = (data) ->
+  @setAllChecked = (data) ->
     if _.isArray(data.value)
       value = data.value
     else
       value = [data.value]
     $("[name='#{data.name}']").val(value)
 
-  @populate: (data) ->
-    switch data.type
-      when FormsJs.InputTypes.TEXT then setValue(data)
-      when FormsJs.InputTypes.RADIO then setChecked(data)
-      when FormsJs.InputTypes.CHECKBOX then setAllChecked(data)
-      when FormsJs.InputTypes.SELECT then setValue(data)
-      when FormsJs.InputTypes.PASSWORD then setValue(data)
-      else setValue(data)
+
