6698ee44ef3947d8b7a69d88cf131d814f145d56
Mike Danaher <danaher.mike@gmail.com>
2014-07-11 17:32:05 -0500

    Fixed issue on required fields for checkboxes or strings with only whitespace

--- .tmp/spec/validators/required.js
+++ .tmp/spec/validators/required.js
@@ -8,11 +8,20 @@
     it('returns false when a text field or select list is required and value is blank', function() {
       return expect(requiredValidator.isValid('')).toBeFalsy();
     });
+    it('returns false when a radio button is required and the value is undefined', function() {
+      return expect(requiredValidator.isValid(void 0)).toBeFalsy();
+    });
+    it('returns false when a checkbox is required and the value is an empty array', function() {
+      return expect(requiredValidator.isValid([])).toBeFalsy();
+    });
+    it('returns false when a text field only has whitespace characters', function() {
+      return expect(requiredValidator.isValid('  ')).toBeFalsy();
+    });
     it('returns true when a text field is required and value is not blank', function() {
       return expect(requiredValidator.isValid('some value')).toBeTruthy();
     });
-    return it('returns false when a radio button or checkbox is required and the value is undefined', function() {
-      return expect(requiredValidator.isValid(void 0)).toBeFalsy();
+    return it('returns true when a checkbox is required and the value is a filled array', function() {
+      return expect(requiredValidator.isValid(['something'])).toBeTruthy();
     });
   });
 
