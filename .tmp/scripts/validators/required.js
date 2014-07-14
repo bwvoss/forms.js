4c652774e0d210fbc5d1e568c2947a5be293c1c2
Mike Danaher <danaher.mike@gmail.com>
2014-07-14 13:02:32 -0500

    Version 0.0.7

--- .tmp/scripts/validators/required.js
+++ .tmp/scripts/validators/required.js
@@ -2,12 +2,38 @@
   namespace('FormsJs.Validator');
 
   FormsJs.Validator.Required = (function() {
+    var FILLED_STRING_REGEXP;
+
+    FILLED_STRING_REGEXP = /\S/;
+
     function Required(options) {
       this.options = options;
     }
 
     Required.prototype.isValid = function(value) {
-      return value !== '' && value !== void 0;
+      return !this.isEmpty(value);
+    };
+
+    Required.prototype.isEmpty = function(value) {
+      switch (typeof value) {
+        case 'number':
+        case 'boolean':
+          return false;
+        case 'string':
+          return this.isEmptyString(value);
+        case 'object':
+          return this.isEmptyObject(value);
+        default:
+          return true;
+      }
+    };
+
+    Required.prototype.isEmptyString = function(value) {
+      return !value.match(FILLED_STRING_REGEXP);
+    };
+
+    Required.prototype.isEmptyObject = function(value) {
+      return value.length === 0;
     };
 
     return Required;
--- scripts/validators/required.coffee
+++ scripts/validators/required.coffee
@@ -2,9 +2,22 @@ namespace('FormsJs.Validator')
 
 class FormsJs.Validator.Required
 
+  FILLED_STRING_REGEXP = /\S/
+
   constructor: (@options) ->
 
   isValid: (value) ->
-    value isnt '' and value isnt undefined
+    !@isEmpty(value)
 
+  isEmpty: (value) ->
+    switch typeof(value)
+      when 'number', 'boolean' then false
+      when 'string' then @isEmptyString(value)
+      when 'object' then @isEmptyObject(value)
+      else true
 
+  isEmptyString: (value) ->
+    !value.match(FILLED_STRING_REGEXP)
+
+  isEmptyObject: (value) ->
+    value.length == 0
